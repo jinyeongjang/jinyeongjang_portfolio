074b29f9cf862a523b8bd284b8064fa47b5b2ebc
jinyeongjang <wlsdud4222@gmail.com>
2024-12-09 00:28:42 +0900

    📦️ Chore: tailwind.config 파일 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,74 +1,81 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: ['class'],
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            textShadow: {
-                sm: '0 1px 2px var(--tw-shadow-color)',
-                DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-                md: '0 4px 8px var(--tw-shadow-color)',
-                lg: '0 8px 16px var(--tw-shadow-color)',
-            },
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)'
-            },
-            colors: {
-                'color-1': 'hsl(var(--color-1))',
-                'color-2': 'hsl(var(--color-2))',
-                'color-3': 'hsl(var(--color-3))',
-                'color-4': 'hsl(var(--color-4))',
-                'color-5': 'hsl(var(--color-5))'
-            },
-            animation: {
-                'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
-                'shiny-text': 'shiny-text 8s infinite',
-                rainbow: 'rainbow var(--speed, 2s) infinite linear',
-                shine: 'shine var(--duration) infinite linear',
-                gradient: 'gradient 8s linear infinite'
-            },
-            keyframes: {
-                'border-beam': {
-                    '100%': {
-                        'offset-distance': '100%'
-                    }
-                },
-                'shiny-text': {
-                    '0%, 90%, 100%': {
-                        'background-position': 'calc(-100% - var(--shiny-width)) 0'
-                    },
-                    '30%, 60%': {
-                        'background-position': 'calc(100% + var(--shiny-width)) 0'
-                    }
-                },
-                rainbow: {
-                    '0%': {
-                        'background-position': '0%'
-                    },
-                    '100%': {
-                        'background-position': '200%'
-                    }
-                },
-                shine: {
-                    '0%': {
-                        'background-position': '0% 0%'
-                    },
-                    '50%': {
-                        'background-position': '100% 100%'
-                    },
-                    to: {
-                        'background-position': '0% 0%'
-                    }
-                },
-                gradient: {
-                    to: {
-                        backgroundPosition: 'var(--bg-size) 0'
-                    }
-                }
-            }
-        }
+const config = {
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        md: '0 4px 8px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {
+        'color-1': 'hsl(var(--color-1))',
+        'color-2': 'hsl(var(--color-2))',
+        'color-3': 'hsl(var(--color-3))',
+        'color-4': 'hsl(var(--color-4))',
+        'color-5': 'hsl(var(--color-5))',
+      },
+      animation: {
+        'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
+        'shiny-text': 'shiny-text 8s infinite',
+        rainbow: 'rainbow var(--speed, 2s) infinite linear',
+        shine: 'shine var(--duration) infinite linear',
+        gradient: 'gradient 8s linear infinite',
+      },
+      keyframes: {
+        'border-beam': {
+          '100%': {
+            'offset-distance': '100%',
+          },
+        },
+        'shiny-text': {
+          '0%, 90%, 100%': {
+            'background-position': 'calc(-100% - var(--shiny-width)) 0',
+          },
+          '30%, 60%': {
+            'background-position': 'calc(100% + var(--shiny-width)) 0',
+          },
+        },
+        rainbow: {
+          '0%': {
+            'background-position': '0%',
+          },
+          '100%': {
+            'background-position': '200%',
+          },
+        },
+        shine: {
+          '0%': {
+            'background-position': '0% 0%',
+          },
+          '50%': {
+            'background-position': '100% 100%',
+          },
+          to: {
+            'background-position': '0% 0%',
+          },
+        },
+        gradient: {
+          to: {
+            backgroundPosition: 'var(--bg-size) 0',
+          },
+        },
+      },
     },
-    plugins: [require("tailwindcss-animate")],
+  },
+  plugins: [require('tailwindcss-animate')],
 };
+
+// ESM 과 CommonJS를 지원하기 위한 코드
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = config;
+}
+
+export default config;
