49b39dc16598684e25680bbbe118eff39e889bf5
jinyeongjang <wlsdud4222@gmail.com>
2025-04-03 19:32:31 +0900

    🎨 Design: Toast 컴포넌트 디자인 수정

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 interface ToastProps {
   message: string;
@@ -8,7 +8,7 @@ interface ToastProps {
 
 const Toast: React.FC<ToastProps> = ({ message }) => {
   return (
-    <div className="fixed top-[32px] left-0 w-full flex justify-center z-50">
+    <div className="fixed left-0 top-[32px] z-50 flex w-full justify-center">
       <motion.div
         className="mt-8"
         initial={{ opacity: 0, y: -20, scale: 0.3 }}
@@ -18,19 +18,17 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
           duration: 0.4,
           ease: [0.4, 0, 0.2, 1],
           scale: {
-            type: 'spring',
+            type: "spring",
             damping: 15,
             stiffness: 200,
           },
-        }}
-      >
+        }}>
         <motion.div
-          className="mb-0 flex flex-col items-center justify-center p-6 w-screen bg-gray-100 text-gray-500 text-3xl font-bold px-6 py-3 rounded-xl shadow-lg"
+          className="mb-0 flex w-screen flex-col items-center justify-center rounded-xl bg-gray-100 p-6 px-6 py-3 text-3xl font-bold text-gray-500 shadow-lg"
           initial={{ y: -10 }}
           animate={{ y: 0 }}
           exit={{ y: -10 }}
-          transition={{ duration: 0.2 }}
-        >
+          transition={{ duration: 0.2 }}>
           {message}
         </motion.div>
       </motion.div>
