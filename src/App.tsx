fcb9e7a15744d9b93062276bc304e89e1a2c5b8c
jinyeongjang <wlsdud4222@gmail.com>
2025-04-03 19:31:09 +0900

    🔥 Edit: App.tsx, main.tsx 페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import Header from './components/Header';
-import ListSection from './components/ListSection';
-import EducationSection from './components/EducationSection';
-import ProjectSection from './components/ProjectSection';
-import AboutSection from './components/AboutSection';
-import { useState, useEffect } from 'react';
-import Toast from './components/ui/toast';
-import { AnimatePresence } from 'framer-motion';
+import Header from "./components/Header";
+import ListSection from "./components/ListSection";
+import EducationSection from "./components/EducationSection";
+import ProjectSection from "./components/ProjectSection";
+import AboutSection from "./components/AboutSection";
 
 function App() {
-  const [showToast, setShowToast] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowToast(false);
-    }, 5000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <div className="text-[20px] custom-cursor">
+    <div className="custom-cursor text-[20px]">
       <Header />
-
-      <AnimatePresence>
-        {showToast && (
-          <Toast
-            message="저의 포트폴리오를 방문해주셔서 감사합니다."
-            onClose={() => setShowToast(false)}
-          />
-        )}
-      </AnimatePresence>
 
       <AboutSection />
       <ProjectSection />
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App.tsx';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
