74edfbb9df2bb6b45199e819a5c80e4afe1dde76
jinyeongjang <wlsdud4222@gmail.com>
2025-04-11 13:06:35 +0900

    🔥 Edit: tailwind.config.js 화면 크기 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ const config = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: { max: "639px" },
+        xxs: { max: "375px" },
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      spacing: {
+        72: "18rem",
+        84: "21rem",
+        96: "24rem",
+      },
+    },
   },
   plugins: [],
 };
