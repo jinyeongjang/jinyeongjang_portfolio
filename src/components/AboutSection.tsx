d84eb38ca0785c6e8f4e4f442de6cd8301c9ae9e
jinyeongjang <wlsdud4222@gmail.com>
2025-04-12 02:27:07 +0900

    🎨 Design: AboutSection 반응형 스타일 수정

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,39 +3,43 @@ import BlurFade from "./ui/blur-fade";
 
 const AboutSection: React.FC = () => {
   return (
-    <section className="relative w-screen overflow-hidden py-10">
+    <section className="relative w-screen py-10">
       <div className="absolute inset-0 bg-gradient-to-br from-blue-200 via-slate-100 to-pink-100" />
       <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-green-200/50 via-transparent to-blue-200/50" />
 
-      <div className="relative z-10 mx-auto max-w-6xl">
+      <div className="relative z-10 mx-auto max-w-6xl px-4 ">
         <div className="flex flex-col items-center justify-between md:flex-row">
           <div className="mb-8 w-full md:mb-0 md:w-1/3">
             <img
               src="./images/my_introduce_picture.jpg"
               alt="introduce_picture"
-              className="mx-auto h-[150px] w-[150px] rounded-full object-cover shadow-lg"
+              className="mx-auto h-[150px] w-[150px] rounded-full object-cover shadow-lg xs:h-[120px] xs:w-[120px] "
             />
           </div>
-          <div className="w-full md:w-2/3 md:pl-8">
+          <div className="w-full ">
             <BlurFade>
-              <h2 className="mb-6 p-4 text-[50px] font-bold text-gray-800 backdrop-blur-[5px]">
+              <h2 className="mb-6 p-4 text-[50px] font-bold text-gray-800 backdrop-blur-[5px] xs:text-[40px]">
                 안녕하세요!
               </h2>
 
-              <p className="z-10 mb-4 p-4 text-[20px] leading-relaxed text-gray-600 backdrop-blur-[5px]">
+              <p className="z-10 mb-4 p-4 text-[20px] leading-relaxed text-gray-600 backdrop-blur-[5px] xs:text-[18px] ">
                 저는 끊임없이 성장하는 프론트엔드 개발자 장진영입니다.
-                <br></br>
+                <br className="hidden sm:block"></br>
                 <span className="relative">
                   <span className="relative z-10">
-                    <strong className="text-[25px] text-slate-600">사용자 경험</strong>
+                    <strong className="text-[25px] text-slate-600 xs:text-[22px] ">
+                      사용자 경험
+                    </strong>
                   </span>
                   <span className="absolute bottom-0 left-0 h-3 w-full bg-yellow-300 opacity-50"></span>
                   <span className="absolute bottom-1 left-0 h-3 w-full bg-yellow-300 opacity-50"></span>
                 </span>
-                을 먼저 생각하며
+                을 먼저 생각하며{" "}
                 <span className="relative">
                   <span className="relative z-10">
-                    <strong className="text-[25px] text-sky-700">새로운 서비스를 만드는 것</strong>
+                    <strong className="text-[25px] text-sky-700 xs:text-[22px] ">
+                      새로운 서비스를 만드는 것
+                    </strong>
                   </span>
                   <span className="absolute bottom-0 left-0 h-3 w-full bg-sky-300 opacity-50"></span>
                   <span className="absolute bottom-1 left-0 h-3 w-full bg-sky-300 opacity-50"></span>
@@ -43,10 +47,10 @@ const AboutSection: React.FC = () => {
                 에 큰 보람을 느낍니다.
               </p>
 
-              <p className="z-10 mb-4 p-4 text-[20px] leading-relaxed text-gray-600 backdrop-blur-[5px]">
+              <p className="z-10 mb-4 p-4 text-[20px] leading-relaxed text-gray-600 backdrop-blur-[5px] xs:text-[18px]">
                 React, Tailwind CSS, javascript, typescript, supabase 를 주로 사용하며,
-                <br></br>
-                최신 웹 기술에 대한 열정과 학습 의지를 가지고 <br></br>
+                <br className="hidden md:block"></br>
+                최신 웹 기술에 대한 열정과 학습 의지를 가지고 <br className="hidden md:block"></br>
                 사용자의 입장에서 편리한 인터페이스 설계와 성능 최적화에 관심이 많습니다.
               </p>
             </BlurFade>
